3ec583371ef5b2feaea014dc1752d22953f91fac
dasartini <adriansartinid@gmail.com>
2024-07-17 16:27:28 +0100

    added code for qr code scanner

--- qr code scanner/src/App.jsx
+++ qr code scanner/src/App.jsx
@@ -1,33 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect } from 'react';
 import './App.css'
+import { Helmet } from 'react-helmet';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+
+  function domReady(fn) {
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+      setTimeout(fn, 1)
+
+    } else {
+      document.addEventListener("DOMContentLoaded", fn)
+    }
+  }
+
+  domReady(function () {
+    let myqr = document.getElementById('qrResult')
+    let lastResult, countResults = 0
+
+    function onScanSuccess(decodeText, decodeResult) {
+      if (decodeText !== lastResult) {
+        ++countResults;
+        lastResult = decodeText;
+
+        alert("Qr: " + decodeText, decodeResult)
+
+        myqr.innerHTML = `you scan ${countResults} : ${decodeText}`
+
+      }
+
+    }
+
+    let htmlscanner = new Html5QrcodeScanner(
+      "qrReader", { fps: 10, qrbox: 250 })
+
+    htmlscanner.render(onScanSuccess)
+
+  })
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Helmet>
+        <script src='https://unpkg.com/html5-qrcode'></script>
+
+      </Helmet>
+
+      <div id='qrResult'>
+        <h1>Scan qr</h1>
+        <div id='qrReader'>
+
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
